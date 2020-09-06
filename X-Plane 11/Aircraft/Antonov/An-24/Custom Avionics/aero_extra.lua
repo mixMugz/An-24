28fea0b9069ea90e1e89fff691df484159e4b905
Maksim N. Brykov <mixmugz@gmail.com>
2020-09-07 01:56:19 +0300

    Removed unneeded drf

--- X-Plane 11/Aircraft/Antonov/An-24/Custom Avionics/aero_extra.lua
+++ X-Plane 11/Aircraft/Antonov/An-24/Custom Avionics/aero_extra.lua
@@ -1,8 +1,8 @@
-defineProperty("gear1_deflect", globalPropertyf("sim/flightmodel2/gear/tire_vertical_deflection_mtr[0]")) -- 0.15 верт отклонение стоянка
+defineProperty("gear1_deflect", globalPropertyf("sim/flightmodel2/gear/tire_vertical_deflection_mtr[0]")) -- 0.15 пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 defineProperty("gear2_deflect", globalPropertyf("sim/flightmodel2/gear/tire_vertical_deflection_mtr[1]"))
 defineProperty("gear3_deflect", globalPropertyf("sim/flightmodel2/gear/tire_vertical_deflection_mtr[2]"))
 
-defineProperty("steer", globalPropertyf("sim/flightmodel2/gear/tire_steer_actual_deg[0]"))
+--defineProperty("steer", globalPropertyf("sim/flightmodel2/gear/tire_steer_actual_deg[0]"))
 
 defineProperty("thrust1", globalPropertyf("sim/flightmodel/engine/POINT_thrust[0]")) -- Newtons
 defineProperty("thrust2", globalPropertyf("sim/flightmodel/engine/POINT_thrust[1]"))
@@ -32,40 +32,40 @@ koef_stabforce_air = 0.8 --0.8
 koef_stabforce_ground = 0.2 --0.2
 koef_propforce = 0 --0
 koef_slipforce = 100 --100
-koef_landgear = 0.5    -- 0ю5 positive nose up
+koef_landgear = 0.5    -- 0пїЅ5 positive nose up
 ------------
 ------------
 ------------
 
 function update()
-	
+
 	delta_thrust = (get(thrust2) + get(thrust3)) - get(thrust1)
 	if get(gear1_deflect) > 0.05 then
 		stab_force = get(thrust2) * koef_stabforce_ground
-		ground_force = delta_thrust * koef_groundforce	
+		ground_force = delta_thrust * koef_groundforce
 	else
-		ground_force = 0 
+		ground_force = 0
 		stab_force = get(thrust2) * koef_stabforce_air
 	end
-	
+
 	if stab_force < 0 then stab_force = 0 end
-	
+
 	thr1 = get(thrust1)
 	thr2 = get(thrust2)
 	if thr1 < 0 then thr1 = 0 end
 	if thr2 < 0 then thr2 = 0 end
 	prop_force = (thr1+ thr2) * koef_propforce
-	
+
 	slip_force = get(slip) * get(ias) * koef_slipforce
 
 	result_roll = prop_force + slip_force
-	result_yaw = ground_force + stab_force 
-	
+	result_yaw = ground_force + stab_force
+
 	set(M_roll, result_roll)
 	set(M_yaw, result_yaw)
-	
+
 	--landgear pitch moment
-	result_pitch = get(ias)^2 * (get(gear1_deploy) + get(gear2_deploy) + get(gear3_deploy))/3 * koef_landgear 
+	result_pitch = get(ias)^2 * (get(gear1_deploy) + get(gear2_deploy) + get(gear3_deploy))/3 * koef_landgear
 	set(M_pitch, result_pitch)
 
 end
