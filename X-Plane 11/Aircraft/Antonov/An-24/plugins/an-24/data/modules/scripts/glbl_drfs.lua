fbcb5634b9043a1b477aa90d7ec8403075dd3fda
Maksim N. Brykov <mixmugz@gmail.com>
2020-09-21 00:46:14 +0300

    New settings datarefs: Reflections/Prop Park Position/Real Headlight

--- X-Plane 11/Aircraft/Antonov/An-24/plugins/an-24/data/modules/scripts/glbl_drfs.lua
+++ X-Plane 11/Aircraft/Antonov/An-24/plugins/an-24/data/modules/scripts/glbl_drfs.lua
@@ -20,7 +20,9 @@ drf_set = {
   kln90b_sec = cGPi(pfx.."set/kln90b_sec"), -- Secondary KLN90B for copilot
   kln_init = cGPi(pfx.."set/kln_init"), -- KLN90B init status
   lang = cGPi(pfx.."set/language"), -- Language selector: 0 - ENG, 1 - RUS
-  reflect = cGPi(pfx.."set/reflections", 1), -- Inside reflections: 0 - disable, 1 - enable (default)
+  park_pos = cGPi(pfx.."set/park_position"), -- Prop park position: 0 - X, 1 - +
+  real_hl = cGPi(pfx.."set/real_headlight"), -- Headlight Taxi/Landing: 0 - instant, 1 - holding
+  reflect = cGPi(pfx.."set/reflections"), -- Inside reflections: 0 - disable, 1 - enable (default)
   switch_rud = cGPi(pfx.."set/switch_rud"), -- switch or hold RUD stopers
 }
 
